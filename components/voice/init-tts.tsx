999d0a9c7fa153aa5a722332db7adf50880c1074
gonggqing <qingssgg@qq.com>
2025-02-04 15:03:56 +0800

    update init tts method

--- components/voice/init-tts.tsx
+++ components/voice/init-tts.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { initTTS } from "@/lib/tts/legacy/oute.ai";
 import { useEffect, useState } from "react";
 import { Dialog, DialogHeader, DialogContent, DialogTitle, DialogDescription } from "@/components/ui/dialog";
 import { SpinnerDotted } from "spinners-react";
 import { Check } from "@phosphor-icons/react";
+import { FishTTS } from "@/lib/tts/fish.ai";
 import "@/styles/wave.css";
 
 export const InitTTSModel = () => {
@@ -12,25 +12,37 @@ export const InitTTSModel = () => {
     const [status, setStatus] = useState<"ready" | "error" | "loading">("loading");
     const [error, setError] = useState<string | null>(null);
 
+    const tts = new FishTTS({
+        apiKey: process.env.FISH_API_KEY,
+        baseUrl: "http://127.0.0.1:8080/v1/tts",
+        streaming: false
+    });
+
     useEffect(() => {
         async function init() {
             setStatus("loading");
             setError(null);
 
-            const res = await initTTS({
-                model_path: "onnx-community/OuteTTS-0.2-500M",
-                language: "en",
-                max_length: 4096
-            })
-
-            if (res.status === "error") {
-                setStatus("error");
-                setError(res.data?.error || "Failed to initialize TTS Model for webgpu, check the webgpu support of your device.");
-            } else {
-                setStatus("ready");
-                setTimeout(() => {
-                    setIsLoading(false);
-                }, 1500);
+            try {
+                const res = await tts.generateSpeech({
+                    text: "TTS is ready",
+                    options: {
+                        format: "wav",
+                        normalize: true,
+                    }
+                })
+    
+                if (res.status !== "success") {
+                    setStatus("error");
+                    setError("Failed to initialize TTS service, check if the tts server is running on http://127.0.0.1:8080");
+                } else {
+                    setStatus("ready");
+                    setTimeout(() => {
+                        setIsLoading(false);
+                    }, 1500);
+                }
+            } catch (error) {
+                console.error(`Error when initilize tts server: ${error}`)
             }
         }
         init();
@@ -42,7 +54,7 @@ export const InitTTSModel = () => {
                 <DialogHeader className="p-8 flex flex-col items-center justify-center gap-4">
                     <DialogTitle className="text-xl font-bold">Initializing TTS Model</DialogTitle>
                     <DialogDescription className="text-base text-muted-foreground">
-                        This will only happen once when you first run the app.
+                        Check if the TTS service is running ...
                     </DialogDescription>
                 </DialogHeader>
                 <div className="relative w-full flex-1 flex flex-col items-center justify-start">
